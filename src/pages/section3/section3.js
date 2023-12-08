1aaf42e0335e4439411ea3e105dfd0e8a351566e
Hojiakbar Name <bekmirzayevhojiakbar306@example.com>
2023-12-08 14:13:49 +0500

    section3 & register page tugadi

--- src/pages/section3/section3.js
+++ src/pages/section3/section3.js
@@ -1,6 +1,20 @@
+import Styles from './section3.module.css';
+import Card from './components/card';
+import Image1 from './img/Vector.png';
+import Image2 from './img/Group.png';
+import Image3 from './img/Component 20 (1).png'
+import Image4 from './img/Group 93.png';
+import Image5 from './img/Group 94 (1).png'
+
 function Section3() {
     return (
-        <div>Section3</div>
+        <div className={Styles.asos}>
+            <Card header={"Регистрация"} image={Image1} footer={"Шаг 1"} />
+            <Card header={"Пополнение счета"} image={Image2} footer={"Шаг 2 "} />
+            <Card header={"Отслеживание по графику"} image={Image3} footer={"Шаг 3"}/>
+            <Card header={"Продажа монет"} image={Image4} footer={"Шаг 4"}/>
+            <Card header={"Вывод прибыли"} image={Image5} footer={"Шаг 5"}/>
+        </div>
     )
 }
 
