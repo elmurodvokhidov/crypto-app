236900c17d6b7ed62fa894c151d046484111162c
islom <islomxonahmedov7@gmail.com>
2023-12-08 14:03:02 +0500

    section1 tugadi

--- src/pages/section1/section1.js
+++ src/pages/section1/section1.js
@@ -1,6 +1,20 @@
+import Styles from './section1.module.css';
+import image1 from './img/img1.png';
+import { NavLink } from 'react-router-dom';
+
 function Section1() {
     return (
-        <div>Section1</div>
+        <div id={Styles.container}>
+            <div className={Styles.box1}>
+                <h1>Ваш надежный криптовалютный серсис недвижимости</h1>
+                <p>С помощью нашего сервиса Вы с легкостью сможете <br /> инвестировать с свое будущее</p>
+                <NavLink to={'/register'} className={Styles.btn1}>Регистрация</NavLink>
+                <NavLink to={'/login'} className={Styles.btn2}>Вход</NavLink>
+            </div>
+            <div className={Styles.box2}>
+                <img src={image1} alt="img1" />
+            </div>
+        </div>
     )
 }
 
